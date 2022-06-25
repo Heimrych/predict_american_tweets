2f4e0a216a7c973ad040fd1b8360a54d1f99b04c
Henrique Fagundes <hfagundes@pinterest.com>
2022-06-25 18:14:35 -0300

    Add K8s files and update Dockerfilex

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,7 @@ COPY requirements.txt requirements.txt
 
 RUN pip3 install -r requirements.txt
 
-# TODO: Change model download to URL
-COPY . .
+COPY predictor.py predictor.py
 
 ENV FLASK_APP=predictor
 
--- predictor.py
+++ predictor.py
@@ -1,12 +1,16 @@
 import pickle
+import urllib.request
 
 from flask import Flask
 from flask import request
 from flask import jsonify
 
+PICKLE_URL = "https://raw.github.com/Heimrych/predict_american_tweets/main/model.pickle"
+
 # PORT = 5013
 app = Flask(__name__)
-app.model = pickle.load(open("model.pickle", "rb"))
+app.model = pickle.load(urllib.request.urlopen(PICKLE_URL))
+# app.model = pickle.load(open("model.pickle", "rb"))
 
 @app.route("/api/american", methods=["POST"])
 def return_prediction():
